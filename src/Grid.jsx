5b47149d6cb886213ed2b5ce3c4bf1b3631747ea
Ryan Johnston <ryjo1026@gmail.com>
2018-08-15 23:50:34 -0400

    Working on refs for square switching

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,16 +1,50 @@
 import React from 'react';
 
-import Subgrid from './Subgrid';
+import Square from './Square';
 
 export default class Grid extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.last = React.createRef();
+
+    this.squareRefs = [];
+    for (let row = 0; row < 9; row += 1) {
+      const cols = [];
+      for (let col = 0; col < 9; col += 1) {
+        cols.push(React.createRef());
+      }
+      this.squareRefs.push(cols);
+    }
+  }
+
+  handleEnter = (event, index) => {
+    // console.log(event.target, index);
+    // if (event.keyCode === 13) {
+    //   event.target.ref.focus();
+    // }
+    this.squareRefs[0][0].current.focus();
+  }
+
   render() {
+    // You can now get a ref directly to the DOM button:
+
     // Generate 2D Array of Subgrids
     const rows = [];
-    for (let row = 0; row < 3; row += 1) {
+    for (let row = 0; row < 9; row += 1) {
       const cols = [];
-      for (let col = 0; col < 3; col += 1) {
+      for (let col = 0; col < 9; col += 1) {
         const index = (3 * row) + col;
-        cols.push(<Subgrid key={index} index={index} />);
+        const square = (
+          <input
+            key={index}
+            type="number"
+            value={index}
+            style={{ width: '50px', height: '50px', border: '1px solid black' }}
+            onKeyDown={(event) => this.handleEnter(event, index)}
+            ref={this.squareRefs[row][col]}
+          />);
+        cols.push(square);
       }
       rows.push(
         <div className="row" key={row} style={{ display: 'flex' }}>
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Square({ index, subgrid }) {
-  return (
-    <input
-      type="number"
-      value={index + (9 * subgrid)}
-      style={{ width: '50px', height: '50px', border: '1px solid black' }}
-    />
-  );
+export default class Square extends React.Component {
+  // TODO dynamically border squares based on index
+  // https://stackoverflow.com/questions/38577224/focus-on-next-field-when-pressing-enter-react-js
+  render() {
+    const { index, handleEnter } = this.props;
+
+    return (
+      <input
+        type="number"
+        value={index}
+        style={{ width: '50px', height: '50px', border: '1px solid black' }}
+        onKeyDown={(event) => handleEnter(event, index)}
+        ref={this.last}
+      />
+    );
+  }
 }
 Square.propTypes = {
-  subgrid: PropTypes.number.isRequired,
+  handleEnter: PropTypes.func.isRequired,
   index: PropTypes.number.isRequired,
 };
