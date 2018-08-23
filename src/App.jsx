34a824ce11796c69e383ff0b66c307a7d3b38513
Ryan Johnston <ryjo1026@gmail.com>
2018-08-22 23:50:26 -0400

    Added checkbox to enable animating solving (not yet implemented)

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,106 @@ import React from 'react';
 import Grid from './Grid';
 import Controls from './Controls';
 
-import solve from './solver';
-
 import './main.css';
+
+/**
+Algorithm:
+  -Search through squares left->right and up->down
+  -Begin to loop through all possible square values
+  -Cross-reference square values with possibilities, first check within quad
+    then row then column
+  -If no match continue to next
+  -Revisit squares if stuck (not always first number possibility)
+
+  What about squares that depend on each other?
+  -Will need to begin to fill in a possibility for a square and then check
+*/
+
+function valueIsInRow(puzzle, row, val) {
+  if (puzzle[row].indexOf(val) > -1) {
+    return true;
+  }
+  return false;
+}
+
+function valueIsInCol(puzzle, col, val) {
+  for (let i = 0; i < puzzle.length; i += 1) {
+    if (puzzle[i][col] === val) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function valueIsInBox(puzzle, row, col, val) {
+  // Get top coordinates for top left corner of the box
+  const boxCol = Math.floor(col / 3) * 3;
+  const boxRow = Math.floor(row / 3) * 3;
+
+  for (let i = 0; i < 3; i += 1) {
+    for (let j = 0; j < 3; j += 1) {
+      if (puzzle[boxRow + i][boxCol + j] === val) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function solveNextSquare(puzzle) {
+  const newPuzzle = puzzle;
+  let row = 0; let col = 0;
+  for (let i = 0; i < puzzle.length; i += 1) {
+    for (let j = 0; j < puzzle[i].length; j += 1) {
+      if (puzzle[i][j] === '') {
+        row = i; col = j;
+      }
+    }
+  }
+
+  // Iterate possible values
+  for (let i = 1; i <= 9; i += 1) {
+    // Check for no same value in row
+    if (!valueIsInRow(puzzle, row, i)
+      && !valueIsInCol(puzzle, col, i)
+      && !valueIsInBox(puzzle, row, col, i)) {
+      // Assign and solve the rest of the puzzle
+      newPuzzle[row][col] = i;
+      if (solve(newPuzzle)) {
+        return true;
+      }
+      // Backtrack if a mistake was made
+      newPuzzle[row][col] = '';
+    }
+  }
+  return false;
+}
+
+function puzzleIsFull(puzzle) {
+  for (let i = 0; i < puzzle.length; i += 1) {
+    for (let j = 0; j < puzzle[i].length; j += 1) {
+      if (puzzle[i][j] === '') {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function solve(puzzle) {
+  if (puzzleIsFull(puzzle)) {
+    return puzzle;
+  }
+
+  if (solveNextSquare(puzzle)) {
+    return puzzle;
+  }
+
+  // Trigger backtrack
+  return false;
+}
+
 
 export default class App extends React.Component {
   constructor(props) {
@@ -40,6 +137,7 @@ export default class App extends React.Component {
 
     this.state = {
       values: squareValues,
+      animate: false,
     };
   }
 
@@ -76,10 +174,16 @@ export default class App extends React.Component {
     // TODO Add arrow key handling
   }
 
+  handleAnimateChange= () => {
+    const { animate } = this.state;
+    this.setState({ animate: !animate });
+  }
+
   handleSubmit = () => {
     const { values } = this.state;
 
     const solution = solve(values);
+
     if (solution) {
       this.setState({ values: solution });
     }
@@ -100,12 +204,16 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { values } = this.state;
+    const { animate, values } = this.state;
 
     return (
       <div className="App">
         <Grid handleEnter={this.handleEnter} squareRefs={this.squareRefs} values={values} />
-        <Controls handleSubmit={this.handleSubmit} />
+        <Controls
+          animate={animate}
+          handleSubmit={this.handleSubmit}
+          handleAnimateChange={this.handleAnimateChange}
+        />
       </div>
     );
   }
--- src/Controls.jsx
+++ src/Controls.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Controls({ handleSubmit }) {
+export default function Controls({ animate, handleSubmit, handleAnimateChange }) {
   return (
     <div className="Controls">
+      <form>
+        <label style={{ display: 'block' }}>
+          Animate solution:
+          <input
+            style={{ display: 'block' }}
+            name="animate"
+            type="checkbox"
+            checked={animate}
+            onChange={handleAnimateChange}
+          />
+        </label>
+      </form>
       <button type="submit" onClick={handleSubmit}>
         Solve Puzzle
       </button>
@@ -11,4 +23,6 @@ export default function Controls({ handleSubmit }) {
 }
 Controls.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
+  handleAnimateChange: PropTypes.func.isRequired,
+  animate: PropTypes.bool.isRequired,
 };
